70f3b40eb8b166075fe0c608a7d23cd4019be9da
AsiyatK <aliciabostan@gmail.com>
2021-01-16 10:18:56 -0500

    initial commit

---
+++ src/main/java/com/galvanize/GMDBapi/GmdbApiApplication.java
@@ -0,0 +1,13 @@
+package com.galvanize.GMDBapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmdbApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GmdbApiApplication.class, args);
+	}
+
+}
